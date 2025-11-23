43bfc74212097bef4f0afb60e1fc7a7780e6d5a9
javiern2023 <javiernavazo@gmail.com>
2025-11-23 12:33:02 +0100

    Primer commit: Proyecto JavaFX

---
+++ EjerciciosJavaFX/src/main/java/com/ejemplo/ejerciciosjavafx/Launcher.java
@@ -0,0 +1,9 @@
+package com.ejemplo.ejerciciosjavafx;
+
+import javafx.application.Application;
+// Clase que lanza la clase principal
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
