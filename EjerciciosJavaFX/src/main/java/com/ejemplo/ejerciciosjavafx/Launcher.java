4b7a86bff86c1ad4187b707be0a11af8fb7dc2d9
javiern2023 <javiernavazo@gmail.com>
2026-02-16 20:32:50 +0100

    JavaFx y Python

--- EjerciciosJavaFX/src/main/java/com/ejemplo/ejerciciosjavafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplo.ejerciciosjavafx;
-
-import javafx.application.Application;
-// Clase que lanza la clase principal
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
